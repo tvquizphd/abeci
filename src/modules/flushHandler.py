0cfc3d9a91b1f7790475d1b4716b00721684dc53
TVQuizPhD <tvquizphd@gmail.com>
2022-04-12 23:22:22 +0000

    add logging for flushing

--- src/modules/flushHandler.py
+++ src/modules/flushHandler.py
@@ -1,3 +1,5 @@
+import logging
+
 class FlushHandler():
 
     EMPTY_MAX = 100
@@ -9,14 +11,18 @@ class FlushHandler():
 
     def flushEmpty(self, config):
         empty = config.empty
-        if self._flushEmpty and len(empty) >= self.EMPTY_MAX:
+        emptyCount = len(empty)
+        if self._flushEmpty and emptyCount >= self.EMPTY_MAX:
+            logging.info(f'Flushing {emptyCount} empty patterns.')
             self._flushEmpty(config)
             return []
         return empty
 
     def flushOutput(self, config):
         output = config.output
+        outputCount = len(output)
         if self._flushOutput and len(output) >= self.OUTPUT_MAX:
+            logging.info(f'Flushing {outputCount} output patterns.')
             self._flushOutput(config)
             return []
         return output
