ef6ddc83ebd7d81e63cad77ec3f59d514cdc1062
TVQuizPhD <tvquizphd@gmail.com>
2022-04-13 03:09:47 +0000

    WORKING: VIRTUAL_ENV=$CONDA_PREFIX pip install --src $VIRTUAL_ENV/src -e .

--- setup.py
+++ setup.py
@@ -27,6 +27,7 @@ setuptools.setup(
         'abeci-pangrams = abeci.pangrams:pangramsCli'
     ]),
     packages=setuptools.find_packages('src'),
-    package_dir={'abeci':'src/abeci'},
+    package_dir={'':'src'},
+    install_requires=['abeci'],
     classifiers=classifiers
 )
