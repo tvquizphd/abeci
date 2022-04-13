9e57b16c241fb1d25180bece34618aa729d8155a
TVQuizPhD <tvquizphd@gmail.com>
2022-04-13 03:45:30 +0000

    add typo data

--- setup.py
+++ setup.py
@@ -27,6 +27,7 @@ setuptools.setup(
         'abeci-pangrams = abeci.pangrams:pangramsCli'
     ]),
     packages=setuptools.find_packages('src'),
+    package_data={'': ['./src/abeci/typo/data/*']},
     package_dir={'':'src'},
     install_requires=['abeci'],
     classifiers=classifiers
